561e4c6e40a2c8696079ee39a1d9e9e789bfc01e
Alan Gervin <alan.gervin@gmail.com>
2021-01-30 15:25:46 -0800

    made first test work the way i wanted it to

--- test/specs/test.js
+++ test/specs/test.js
@@ -7,18 +7,15 @@ var assert = require('assert');
 
 describe('Title', function() {
   describe('#TitleImg has logo or splash image named logo.jpg', function() {
-    it('#TitleImg contains images/logo.jpg', function() {
+    it('#TitleImg contains logo.jpg', function() {
       browser.url('/');
       const image = $('#TitleImg');
-      image.getAttribute('src');
-
-      assert.equal(image.getAttribute('src'),"http://localhost:5500/images/logo.jpg")
+      let link = image.getAttribute('src');
+      console.log(typeof link);
+      let answer = link.includes('logo.jpg');
+      assert.equal(answer, true);
     })
-    it('can handle commands synchronously', () => {
-      let value = $('#TitleImg').getValue()
-      console.log(value) // outputs: some value
-    })
-
   });
+
 });
 
