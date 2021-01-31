292a2373de02e383a5e5b1569532c074ef478e88
Alan Gervin <alan.gervin@gmail.com>
2021-01-30 17:34:42 -0800

    added more tests

--- test/specs/test.js
+++ test/specs/test.js
@@ -13,9 +13,55 @@ describe('Title', function() {
       let link = image.getAttribute('src');
       console.log(typeof link);
       let answer = link.includes('logo.jpg');
-      assert.equal(answer, true);
-    })
+      assert.strictEqual(answer, true);
+    });
   });
-
+  describe('Title is correct', function() {
+    it('#Title text is strictEqual to Emerald Company', function() {
+      browser.url('/');
+      const title = $('#Title').getText();
+      assert.strictEqual(title,"Emerald Company");
+    });
+  });
 });
 
+
+describe('Mission Statement', function() {
+  describe('Mission statement is correct', function() {
+    it('#missionStatement has correct statement', function() {
+      browser.url("/");
+      const missionStatement = $('#missionStatement');
+      assert.strictEqual(missionStatement.getText(),"Buy gems")
+    });
+  });
+});
+
+describe("A list of products", function() {
+  describe('unordered list items exists', function() {
+    it('#items exits', function() {
+      browser.url('/');
+      const list = $('#items');
+      const err = list.error;
+      console.log(err);
+      assert.strictEqual(err,undefined);
+    })
+  })
+})
+
+describe("Teammates", function() {
+  describe("has flexbox named teammates", function() {
+    it("#teammates is a flexbox", function() {
+      let teammates = $("#teammates");
+      let err = teammates.error;
+      assert.strictEqual(err, undefined);
+    })
+  })
+  describe("has display property is set to flex", function() {
+    it("#teammates.display equal to flex", function() {
+      let teammates = $("#teammates");
+      let classAttrs = teammates.getAttribute('class');
+      let answer = classAttrs.includes('flex');
+      assert.strictEqual(answer, true);
+    })
+  })
+})
